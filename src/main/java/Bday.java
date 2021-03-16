e1a1446566f8946799a88cf263f97ae8d608b13d
Dominik <stelmaszynski.dominik@gmail.com>
2021-03-16 18:15:55 +0100

    greeting function

--- src/main/java/Bday.java
+++ src/main/java/Bday.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Bday {
 
     public static void main(String[] args) {
 
+        System.out.println("Hi!");
+        Scanner input = new Scanner(System.in);
+
+        String correctHi = "Hi";
+        String greatings = input.nextLine();
+        if (greatings.equals(correctHi)) {
+            System.out.println("Nice to meet You. So it's Your Bday today?");
+        } else {
+            System.out.println("Can u at least say 'Hi'??");
+            }
+        }
     }
-}
