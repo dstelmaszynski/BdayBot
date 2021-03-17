6d1e3b1efe640942009c66e2b7690d415f2cb30c
Dominik <stelmaszynski.dominik@gmail.com>
2021-03-17 18:11:27 +0100

    added IgnoreCase function

--- src/main/java/Bday.java
+++ src/main/java/Bday.java
@@ -1,6 +1,7 @@
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Bday {
 
@@ -9,11 +10,12 @@ public class Bday {
         System.out.println("Hi!");
         Scanner input = new Scanner(System.in);
 
-        Set<String> greetings = new HashSet<String>();
+        Set<String> greetings = new TreeSet<String>(String.CASE_INSENSITIVE_ORDER);
         greetings.add("Hi");
         greetings.add("Hello");
         greetings.add("Hey");
         greetings.add("What's up");
+
         String textInput = input.nextLine();
         if (greetings.contains(textInput)) {
             System.out.println("Nice to meet You. So it's Your Bday today?\n" + "Yes or No?");
@@ -21,9 +23,9 @@ public class Bday {
             System.out.println("Can u at least say 'Hi'??");
             }
         textInput = input.nextLine();
-        if (textInput.equals("Yes")) {
+        if (textInput.equalsIgnoreCase("Yes")) {
             System.out.println("Happy Bday! I wish You all best!");
-        }else if (textInput.equals("No")) {
+        }else if (textInput.equalsIgnoreCase("No")) {
             System.out.println("Well, sorry to bother You! Good luck anyway!");
         }else {
             System.out.println("Sorry, I don't get it. So it's Your Bday today?\n" + "Yes or No?");
