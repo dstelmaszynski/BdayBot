440af651305202bb9b5120780e7f25276e1d5792
Dominik <stelmaszynski.dominik@gmail.com>
2021-03-17 17:46:05 +0100

    added set storage of greetings

--- src/main/java/Bday.java
+++ src/main/java/Bday.java
@@ -1,4 +1,6 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Bday {
 
@@ -7,12 +9,24 @@ public class Bday {
         System.out.println("Hi!");
         Scanner input = new Scanner(System.in);
 
-        String correctHi = "Hi";
-        String greetings = input.nextLine();
-        if (greetings.equals(correctHi)) {
-            System.out.println("Nice to meet You. So it's Your Bday today?");
+        Set<String> greetings = new HashSet<String>();
+        greetings.add("Hi");
+        greetings.add("Hello");
+        greetings.add("Hey");
+        greetings.add("What's up");
+        String textInput = input.nextLine();
+        if (greetings.contains(textInput)) {
+            System.out.println("Nice to meet You. So it's Your Bday today?\n" + "Yes or No?");
         } else {
             System.out.println("Can u at least say 'Hi'??");
             }
+        textInput = input.nextLine();
+        if (textInput.equals("Yes")) {
+            System.out.println("Happy Bday! I wish You all best!");
+        }else if (textInput.equals("No")) {
+            System.out.println("Well, sorry to bother You! Good luck anyway!");
+        }else {
+            System.out.println("Sorry, I don't get it. So it's Your Bday today?\n" + "Yes or No?");
+        }
         }
     }
